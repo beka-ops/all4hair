ec367bf99714c51178a164d2e16c95266214758d
beka-ops <rh4207@nyu.edu>
2026-04-01 20:15:19 -0400

    Modify App.py with Railway DB Connection pt 3

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ db = mysql.connector.connect(
     host="mysql.railway.internal",
     port=3306,
     user="root",
-    password="uKSShOJARvmovKVAfmFDwTRigYuVxItu",
+    password="IZvzxNoSLdklFQnVjANxvxClFxRENfcO",
     database="railway"
 )
 
