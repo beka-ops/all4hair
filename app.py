75cf82fddaf97116290fcf08a68927a296a8c2a0
beka-ops <rh4207@nyu.edu>
2026-04-04 22:38:23 -0400

    update cursor to keep db alive

--- app.py
+++ app.py
@@ -11,9 +11,14 @@ db = mysql.connector.connect(
     database="railway"
 )
 
+def get_cursor():
+    db.reconnect()
+    return db.cursor()
+
 @app.route('/')
 def home():
     return redirect(url_for('view_customer'))
+
 @app.route('/c_register', methods=['GET', 'POST'])
 def c_register():
     if request.method == 'POST':
@@ -21,7 +26,7 @@ def c_register():
         lname_data = request.form['lastName']
         email_data = request.form['emailAddress']
         number_data = request.form['phoneNumber']
-        cursor = db.cursor()
+        cursor = get_cursor()
         cursor.execute("insert into customer (firstName, lastName,emailAddress, phoneNumber) values (%s, %s, %s, %s)", (fname_data,lname_data,email_data,number_data ))
         db.commit()
         return redirect(url_for('view_customer'))
@@ -31,7 +36,7 @@ def c_register():
 @app.route('/view_customer', methods=['GET', 'POST'])
 def view_customer():
     if request.method == 'GET':
-        cursor = db.cursor()
+        cursor = get_cursor()
         cursor.execute("select * from customer")
         results = cursor.fetchall()
         return render_template('customers.html', customers=results)
@@ -41,7 +46,7 @@ def view_customer():
 @app.route('/delete_customer', methods=['GET', 'POST'])
 def delete_customer():
     if request.method == 'POST':
-        cursor = db.cursor()
+        cursor = get_cursor()
         cursor.execute("delete from customer where customer_id=%s", (request.form['customer_id'], ))
         db.commit()
         results = cursor.fetchall()
@@ -50,11 +55,11 @@ def delete_customer():
 @app.route('/c_modify', methods=['GET', 'POST'])
 def c_modify():
     if request.method == 'GET':
-        cursor = db.cursor()
+        cursor = get_cursor()
         cursor.execute("select * from customer where customer_id=%s", (request.args.get('customer_id'), ))
         return render_template('c_edit.html', customer=cursor.fetchall())
     elif request.method == 'POST':
-        cursor = db.cursor()
+        cursor = get_cursor()
         cursor.execute("update customer set firstName=%s, lastName=%s, emailAddress=%s, phoneNumber=%s "
                        "where customer_id=%s", (request.form['firstName'], request.form['lastName'], request.form['emailAddress'] , request.form['phoneNumber'] ,request.form['customer_id']))
         db.commit()
